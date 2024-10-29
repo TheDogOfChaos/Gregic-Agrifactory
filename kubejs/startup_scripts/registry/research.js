58c439e4639039cd777ed6248f24d958db5debac
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-10-29 17:41:21 +0000

    casual asset rip

--- kubejs/startup_scripts/registry/research.js
+++ kubejs/startup_scripts/registry/research.js
@@ -2,7 +2,7 @@ StartupEvents.registry('item', e => {
     // 1x packs
     e.create('automation_science_pack')
     e.create('logistic_science_pack')
-    e.create('agricultural_science_pack')
+    e.create('agriculture_science_pack')
     e.create('chemical_science_pack')
     e.create('production_science_pack')
     e.create('utility_science_pack')
@@ -11,7 +11,7 @@ StartupEvents.registry('item', e => {
     // 8x packs
     e.create('automation_science_pack_8')
     e.create('logistic_science_pack_8')
-    e.create('agricultural_science_pack_8')
+    e.create('agriculture_science_pack_8')
     e.create('chemical_science_pack_8')
     e.create('production_science_pack_8')
     e.create('utility_science_pack_8')
@@ -20,7 +20,7 @@ StartupEvents.registry('item', e => {
     // 64x packs
     e.create('automation_science_pack_64')
     e.create('logistic_science_pack_64')
-    e.create('agricultural_science_pack_64')
+    e.create('agriculture_science_pack_64')
     e.create('chemical_science_pack_64')
     e.create('production_science_pack_64')
     e.create('utility_science_pack_64')

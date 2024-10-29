ea22b76ba2e1cc29946bc54871ae908dd63954cf
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-10-29 16:03:41 +0000

    BE GIN

--- kubejs/startup_scripts/items/research.js
+++ kubejs/startup_scripts/items/research.js
@@ -1,1 +1,28 @@
-StartupEvents.registry
+StartupEvents.registry('item', e => {
+    // 1x packs
+    e.create('automation_science_pack')
+    e.create('logistic_science_pack')
+    e.create('agricultural_science_pack')
+    e.create('chemical_science_pack')
+    e.create('production_science_pack')
+    e.create('utility_science_pack')
+    e.create('quantum_science_pack')
+
+    // 8x packs
+    e.create('automation_science_pack_8')
+    e.create('logistic_science_pack_8')
+    e.create('agricultural_science_pack_8')
+    e.create('chemical_science_pack_8')
+    e.create('production_science_pack_8')
+    e.create('utility_science_pack_8')
+    e.create('quantum_science_pack_8')
+
+    // 64x packs
+    e.create('automation_science_pack_64')
+    e.create('logistic_science_pack_64')
+    e.create('agricultural_science_pack_64')
+    e.create('chemical_science_pack_64')
+    e.create('production_science_pack_64')
+    e.create('utility_science_pack_64')
+    e.create('quantum_science_pack_64')
+})

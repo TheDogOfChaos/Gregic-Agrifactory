c0e2cae2c39f4d18cc3ddc5c7d57c1c10af6ba90
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-11-09 23:26:55 +0000

    add greate, nomoportals and nomowanderer

---
+++ kubejs/client_scripts/jei.js
@@ -0,0 +1,33 @@
+JEIEvents.hideItems(event => {//begin the massacre
+
+    // Greate/Create
+    { // greate
+        event.hide(/greate:large_.*_cogwheel/)
+        event.hide(/greate:large_.*_.*_cogwheel/)
+        event.hide(/greate:large_.*_.*_.*_cogwheel/)
+
+        event.hide(/greate:.*_crushing_wheel/)
+        event.hide(/greate:.*_.*_crushing_wheel/)
+        event.hide(/greate:.*_.*_.*_crushing_wheel/)
+
+        event.hide(/greate:.*_encased_fan/)
+        event.hide(/greate:.*_.*_encased_fan/)
+        event.hide(/greate:.*_.*_.*_encased_fan/)
+
+        event.hide(/greate:.*_mechanical_press/)
+        event.hide(/greate:.*_.*_mechanical_press/)
+        event.hide(/greate:.*_.*_.*_mechanical_press/)
+
+        event.hide(/greate:.*_mechanical_mixer/)
+        event.hide(/greate:.*_.*_mechanical_mixer/)
+        event.hide(/greate:.*_.*_.*_mechanical_mixer/)
+
+        event.hide(/greate:.*_millstone/)
+        event.hide(/greate:.*_.*_millstone/)
+        event.hide(/greate:.*_.*_.*_millstone/)
+
+        event.hide(/greate:.*_mechanical_saw/)
+        event.hide(/greate:.*_.*_mechanical_saw/)
+        event.hide(/greate:.*_.*_.*_mechanical_saw/)
+    }
+})

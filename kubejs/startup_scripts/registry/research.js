cac9ccfa4ecad37c7a5c66d8e38e47398a59d9e9
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-11-10 16:08:00 +0000

    tech cards

--- kubejs/startup_scripts/registry/research.js
+++ kubejs/startup_scripts/registry/research.js
@@ -1,13 +1,13 @@
 StartupEvents.registry('item', e => {
     // 1x
-    e.create('basic_tech_card')
-    e.create('automation_tech_card')
-    e.create('logistic_tech_card')
-    e.create('agriculture_tech_card')
-    e.create('chemical_tech_card')
-    e.create('production_tech_card')
-    e.create('utility_tech_card')
-    e.create('quantum_tech_card')
+    e.create('basic_tech_card').texture('kubejs:item/tech_cards/basic_tech_card')
+    e.create('automation_tech_card').texture('kubejs:item/tech_cards/automation_tech_card')
+    e.create('logistic_tech_card').texture('kubejs:item/tech_cards/logistic_tech_card')
+    e.create('agriculture_tech_card').texture('kubejs:item/tech_cards/agriculture_tech_card')
+    e.create('chemical_tech_card').texture('kubejs:item/tech_cards/chemical_tech_card')
+    e.create('production_tech_card').texture('kubejs:item/tech_cards/production_tech_card')
+    e.create('utility_tech_card').texture('kubejs:item/tech_cards/utility_tech_card')
+    e.create('quantum_tech_card').texture('kubejs:item/tech_cards/quantum_tech_card')
 
     // 8x
     e.create('basic_tech_card_8x')

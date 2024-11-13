84cee8fe9132e8209116b467ae93d9fc8ebd88cd
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-11-13 01:10:27 +0000

    oop

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -1,7 +1,7 @@
 JEIEvents.hideItems(event => {//begin the massacre
 
-    // Greate/Create
-    if (Platform.isLoaded('greate') && false /*DO NOT SET TO TRUE ATM*/) { // greate
+    //* Greate/Create
+    if (false) { // greate is borked rn, wait until next release
         event.hide(/greate:large_.*_cogwheel/)
         event.hide(/greate:large_.*_.*_cogwheel/)
         event.hide(/greate:large_.*_.*_.*_cogwheel/)
@@ -31,15 +31,22 @@ JEIEvents.hideItems(event => {//begin the massacre
         event.hide(/greate:.*_.*_.*_mechanical_saw/)
     }
     event.hide(['create:water_wheel', 'create:large_water_wheel', 'create:hand_crank', 'create:steam_engine', 'create:windmill_bearing']) //NoMoRPM
-})
 
-JEIEvents.addItems(event => {
+    //* Mystical Agriculture
     // all Mystical Agriculture seeds.
     let maSeeds = Item.getTypeList.filter(id => (id.includes("mysticalagriculture") && id.includes("seeds")))
 
+    // Ore seeds created in startup_scripts\registry\mysticalAgriculture.js
+    // this array will be expanded over time as new ore seeds are added
     let oreSeeds = ['mysticalagriculture:magnetite_ore_seeds']
 
-    maSeeds = maSeeds.filter(item => !maSeeds.includes(item))
-    
+    // we want to keep the ore seeds, so filter them out of maSeeds
+    maSeeds = maSeeds.filter(item => !oreSeeds.includes(item))
+
+    // KILL EM ALL!!
     maSeeds.forEach(item => {event.hide(item)});
+})
+
+JEIEvents.addItems(event => {
+    
 })

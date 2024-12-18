d9eb37879f551a1a41e1e4c1cf4af1c865ebbc42
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-12-18 23:07:17 +0000

    updage

--- kubejs/server_scripts/recipes/remove.js
+++ kubejs/server_scripts/recipes/remove.js
@@ -3,17 +3,12 @@
 
 ServerEvents.recipes(event => {
     // Create
-    event.remove({output:"create:steam_engine"})
-    event.remove({output:"create:windmill_bearing"})
-    event.remove({output:"create:hand_crank"})
-    event.remove({output:"create:water_wheel"})
-    event.remove({output:"create:large_water_wheel"})
+    event.remove({output:"create:steam_engine"});
+    event.remove({output:"create:windmill_bearing"});
+    event.remove({output:"create:hand_crank"});
+    event.remove({output:"create:water_wheel"});
+    event.remove({output:"create:large_water_wheel"});
 
-    // Mystical Agriculture 
-    event.remove({type: "mysticalagriculture:awakening"})
-    event.remove({type: "mysticalagriculture:infusion"})
-    event.remove({type: "mysticalagriculture:enchanter"})
-    event.remove({type: "mysticalagriculture:soul_extraction"})
-    event.remove({type: "mysticalagriculture:soulium_spawner"})
-    event.remove({type: "mysticalagriculture:reprocessor"})
+    // The absolute ENTIRITY of Gregtech.
+    event.remove({mod:"gtceu"});
 })

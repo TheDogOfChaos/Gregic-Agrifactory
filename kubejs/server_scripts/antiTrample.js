be6232e4ee28290e86a33c55244a1337fc740072
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-12-21 13:07:18 +0000

    dont need these

--- kubejs/server_scripts/antiTrample.js
+++
@@ -1,5 +0,0 @@
-BlockEvents.farmlandTrampled(event => {
-    if(event.player.feetArmorItem == 'kubejs:anti_trample_boots'){
-        event.cancel()
-    }
-})
--- kubejs/startup_scripts/registry/armor.js
+++
@@ -1,4 +0,0 @@
-StartupEvents.registry('item', event => {
-    event.create('anti_trample_boots', 'boots')
-        .tier('leather')
-})

fb825ded955bea19bbf7284c4205910a83eeffcc
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-12-24 14:23:52 +0000

    hmmm

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -4,33 +4,35 @@
 JEIEvents.hideItems(event => {//begin the massacre
 
     //* Greate/Create
-    event.hide(/greate:large_.*_cogwheel/)
-    event.hide(/greate:large_.*_.*_cogwheel/)
-    event.hide(/greate:large_.*_.*_.*_cogwheel/)
+    if(Platform.isLoaded("greate")){
+        event.hide(/greate:large_.*_cogwheel/)
+        event.hide(/greate:large_.*_.*_cogwheel/)
+        event.hide(/greate:large_.*_.*_.*_cogwheel/)
 
-    event.hide(/greate:.*_crushing_wheel/)
-    event.hide(/greate:.*_.*_crushing_wheel/)
-    event.hide(/greate:.*_.*_.*_crushing_wheel/)
+        event.hide(/greate:.*_crushing_wheel/)
+        event.hide(/greate:.*_.*_crushing_wheel/)
+        event.hide(/greate:.*_.*_.*_crushing_wheel/)
 
-    event.hide(/greate:.*_encased_fan/)
-    event.hide(/greate:.*_.*_encased_fan/)
-    event.hide(/greate:.*_.*_.*_encased_fan/)
+        event.hide(/greate:.*_encased_fan/)
+        event.hide(/greate:.*_.*_encased_fan/)
+        event.hide(/greate:.*_.*_.*_encased_fan/)
 
-    event.hide(/greate:.*_mechanical_press/)
-    event.hide(/greate:.*_.*_mechanical_press/)
-    event.hide(/greate:.*_.*_.*_mechanical_press/)
+        event.hide(/greate:.*_mechanical_press/)
+        event.hide(/greate:.*_.*_mechanical_press/)
+        event.hide(/greate:.*_.*_.*_mechanical_press/)
 
-    event.hide(/greate:.*_mechanical_mixer/)
-    event.hide(/greate:.*_.*_mechanical_mixer/)
-    event.hide(/greate:.*_.*_.*_mechanical_mixer/)
+        event.hide(/greate:.*_mechanical_mixer/)
+        event.hide(/greate:.*_.*_mechanical_mixer/)
+        event.hide(/greate:.*_.*_.*_mechanical_mixer/)
 
-    event.hide(/greate:.*_millstone/)
-    event.hide(/greate:.*_.*_millstone/)
-    event.hide(/greate:.*_.*_.*_millstone/)
+        event.hide(/greate:.*_millstone/)
+        event.hide(/greate:.*_.*_millstone/)
+        event.hide(/greate:.*_.*_.*_millstone/)
 
-    event.hide(/greate:.*_mechanical_saw/)
-    event.hide(/greate:.*_.*_mechanical_saw/)
-    event.hide(/greate:.*_.*_.*_mechanical_saw/)
+        event.hide(/greate:.*_mechanical_saw/)
+        event.hide(/greate:.*_.*_mechanical_saw/)
+        event.hide(/greate:.*_.*_.*_mechanical_saw/)
+    }
     
     //NoMoRPM 
     event.hide([
@@ -44,4 +46,9 @@ JEIEvents.hideItems(event => {//begin the massacre
 
 JEIEvents.addItems(event => {
     // bingus
+})
+
+JEIEvents.removeCategories(event => {
+    console.log(event.categoryIds)
+
 })

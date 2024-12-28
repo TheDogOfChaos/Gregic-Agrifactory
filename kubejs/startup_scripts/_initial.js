bd59559a7dfff8eb02f33ace96fca2eb1d41616b
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-12-28 19:31:19 +0000

    trade station is fucked

--- kubejs/startup_scripts/_initial.js
+++ kubejs/startup_scripts/_initial.js
@@ -7,5 +7,5 @@
  * @value A short reason of the issues that the block/item is causing.
  */
 global.problematicBlocks = new Map([
-    ['forestry:trade_station', 'Its GUI is currently nonfunctional, and §l§nCANNOT§r be closed without opening another GUI such as FTB Quests.']
+    ['forestry:trade_station', 'Its GUI is currently nonfunctional, and §l§nCANNOT§r be closed without opening another GUI such as FTB Quests (ftb chunks/teams do not work as a substitute for this).']
 ]);

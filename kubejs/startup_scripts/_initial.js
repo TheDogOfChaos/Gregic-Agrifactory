ce55f3104c655037bc897ae73650e5d1c0638b51
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-12-28 19:32:57 +0000

    clarity

--- kubejs/startup_scripts/_initial.js
+++ kubejs/startup_scripts/_initial.js
@@ -1,5 +1,5 @@
 // priority: 999
-//! This script loads first before the other startup scripts
+//! This script loads first before ALL other startup scripts
 // put your util or patches here so that you can access them at other scripts
 
 /** A Map of blocks/items that are not functioning as intended.
@@ -7,5 +7,5 @@
  * @value A short reason of the issues that the block/item is causing.
  */
 global.problematicBlocks = new Map([
-    ['forestry:trade_station', 'Its GUI is currently nonfunctional, and §l§nCANNOT§r be closed without opening another GUI such as FTB Quests (ftb chunks/teams do not work as a substitute for this).']
+    ['forestry:trade_station', "Its GUI is currently nonfunctional, and §l§nCANNOT§r be closed without opening FTB Quests's GUI."]
 ]);

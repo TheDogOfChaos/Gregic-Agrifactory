8c5b1f38983b048aeb1b1bc2abc9ccbf862a305a
Becquerel <46350316+TheDogOfChaos@users.noreply.github.com>
2024-12-28 19:29:55 +0000

    almostUnified

--- kubejs/startup_scripts/_initial.js
+++ kubejs/startup_scripts/_initial.js
@@ -1,3 +1,11 @@
 // priority: 999
 //! This script loads first before the other startup scripts
 // put your util or patches here so that you can access them at other scripts
+
+/** A Map of blocks/items that are not functioning as intended.
+ * @key The Id of the broken/bugged/problematic block/item.
+ * @value A short reason of the issues that the block/item is causing.
+ */
+global.problematicBlocks = new Map([
+    ['forestry:trade_station', 'Its GUI is currently nonfunctional, and §l§nCANNOT§r be closed without opening another GUI such as FTB Quests.']
+]);
